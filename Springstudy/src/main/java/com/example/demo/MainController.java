e9463ad07f3b730a908a1c997538c82ea0e64a96
kumaken9503 <81018330+kumaken9503@users.noreply.github.com>
2021-04-15 13:55:07 +0900

    Study

---
+++ Springstudy/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+@Controller
+public class MainController {
+
+@RequestMapping(value="/")
+public ModelAndView indexPost(ModelAndView mv){
+ArrayList<String[]> customers = new ArrayList<String[]>();
+customers.add(new String[] {"佐藤HTML太郎","35歳","男性"});
+customers.add(new String[] {"鈴木Java五郎","24歳","男性"});
+customers.add(new String[] {"高橋CSS子","29歳","女性"});
+mv.addObject("customers", customers);
+mv.setViewName("index");
+return mv;
+}
+}
